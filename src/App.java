013e0481db00889dbdfec4fbc41baab05ec9e7ed
RobsonSousaS <robson.silva09@aluno.ifce.edu.br>
2023-08-15 15:04:30 -0300

    BANCO_ATIVIDADE_ALEX

--- src/App.java
+++ src/App.java
@@ -1,18 +1,59 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        Conta c1 = new Conta("", "", "78388", 100.0);
-        Conta c2 = new Conta("", "", "78388", 0.0);
+import java.util.Scanner;
 
-        System.out.println("USUARIO: " + c1.getTitular() + " SALDO: " + c1.getSaldo());
-        depositar(c1, 100.0);
-        System.out.println("USUARIO: " + c1.getTitular() + " SALDO: " + c1.getMostrarSaldo());
-        System.out.println("-------------------------------");
-        sacar(c1, 50.0);
-        System.out.println("USUARIO: " + c1.getTitular() + " SALDO: " + c1.getMostrarSaldo());
-        System.out.println("-------------------------------");
-        transferir(c1, c2, 50.0);
-        System.out.println("USUARIO: " + c2.getTitular() + " SALDO: " + c2.getMostrarSaldo());
-        System.out.println("USUARIO: " + c1.getTitular() + " SALDO: " + c1.getMostrarSaldo());
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Conta c1 = new Conta("Titular 1", "12345", "Agencia A", 100.0);
+        Conta c2 = new Conta("Titular 2", "67890", "Agencia B", 0.0);
+
+        int opcao;
+        
+      
+            System.out.println("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+            System.out.println("|| BEM VINDO AO BANCO DA PELADA, QUAL OPERACAO DESEJA FAZER? ||");
+            System.out.println("|| 1° CRIAR NOVA CONTA                                       ||");
+            System.out.println("|| 2° VER SALDO DE SUA CONTA                                 ||");
+            System.out.println("|| 3° DEPOSITAR EM SUA CONTA                                 ||");
+            System.out.println("|| 4° SACAR DE SUA CONTA                                     ||");
+            System.out.println("|| 5° TRANSFERIR PARA OUTRA CONTA                            ||");
+            System.out.println("|| 0° SAIR DO BANCO                                          ||");
+            System.out.println("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+
+          do {
+            System.out.println("SELECIONE UMA OPCAO:");
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                System.out.println("SERA ADICIONADO EM UMA VERSAO POSTERIOR, PEDIMOS DESCULPAS!");
+                    break;
+                case 2:
+                    System.out.println(c1.getSaldo());
+                    break;
+                case 3:
+                    System.out.println("Digite o valor para depositar:");
+                    double valorDeposito = scanner.nextDouble();
+                    depositar(c1, valorDeposito);
+                    break;
+                case 4:
+                    System.out.println("Digite o valor para sacar:");
+                    double valorSaque = scanner.nextDouble();
+                    sacar(c1, valorSaque);
+                    break;
+                case 5:
+                    System.out.println("Digite o valor para transferir:");
+                    double valorTransferencia = scanner.nextDouble();
+                    transferir(c1, c2, valorTransferencia);
+                    break;
+                case 0:
+                    System.out.println("Saindo do banco. Obrigado!");
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        } while (opcao != 0);
+
+        scanner.close();
     }
 
     public static void depositar(Conta conta, double valor) {
--- src/Conta.java
+++ src/Conta.java
@@ -1,54 +1,41 @@
-public class Conta {
+class Conta {
+    private String nomeTitular;
+    private String numeroConta;
     private String agencia;
-    private String numero;
     private double saldo;
-    private String titular;
 
-    public Conta(){
-
-    }
-
-    public Conta(String titular, String agencia, String numero, double saldo) {
-        this.titular = titular;
+    public Conta(String nomeTitular, String numeroConta, String agencia, double saldoInicial) {
+        this.nomeTitular = nomeTitular;
+        this.numeroConta = numeroConta;
         this.agencia = agencia;
-        this.numero = numero;
-        this.saldo = saldo;
-    }
-
-    public String getTitular() {
-        return titular;
-    }
-
-    public void setTitular(String titular) {
-        this.titular = titular;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public double getMostrarSaldo() {
-        return saldo;
+        this.saldo = saldoInicial;
     }
 
     public void depositar(double valor) {
         saldo += valor;
+        System.out.println("Depósito de R$" + valor + " realizado. Novo saldo: R$" + saldo);
     }
 
     public void sacar(double valor) {
-        if (saldo >= valor) {
+        if (valor <= saldo) {
             saldo -= valor;
+            System.out.println("Saque de R$" + valor + " realizado. Novo saldo: R$" + saldo);
         } else {
             System.out.println("Saldo insuficiente para saque.");
         }
     }
 
     public void transferir(Conta destino, double valor) {
-        if (saldo >= valor) {
+        if (valor <= saldo) {
             saldo -= valor;
             destino.depositar(valor);
+            System.out.println("Transferência de R$" + valor + " realizada para a conta " + destino.numeroConta);
         } else {
             System.out.println("Saldo insuficiente para transferência.");
         }
+    }
+
+    public double getSaldo() {
+        return saldo;
     }
 }
